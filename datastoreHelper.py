8c35c021dd9f88b2a765cc04556376987787669f
ZaryHicks <zaryhicks@gmail.com>
2020-02-22 15:57:57 -0500

    Add sign in and sign up, with database support

---
+++ datastoreHelper.py
@@ -0,0 +1,143 @@
+from google.cloud import datastore
+
+# must import the relevant objects
+import dataClasses
+
+# this is a helper class that main.py uses to handle calls to the datastore
+
+# gets the datastore client object
+def get_client():
+    return datastore.Client()
+
+
+# logs msg (print) for debugging
+def log(msg):
+    print('datastore: %s' % msg)
+
+
+# writes this User and password hash to the datastore
+def save_user(user, passwordhash):
+    # get datastore client
+	client = get_client()
+	
+	# create entity - needs a datastore key, which we get from load_key
+	# pass in client, kind, id
+    entity = datastore.Entity(load_key(client, 'User', user.username))
+    
+	# key is set, now set entity values from the user object and passwordhash
+	entity['username'] = user.username
+	entity['firstname'] = user.firstname
+    entity['email'] = user.email
+    entity['passwordhash'] = passwordhash
+    
+	# save the entity to the datastore
+	client.put(entity)
+	
+
+# check the datastore for a user with matching username and passhash (hashword)
+def load_user(username, passwordhash):
+    """Load a user based on the passwordhash; if the passwordhash doesn't match
+    the username, then this should return None."""
+
+	# get datastore client
+    client = get_client()
+	
+	# create a datastore query for kind username, with username and passhash filters
+    q = client.query(kind='User')
+    q.add_filter('username', '=', username)
+    q.add_filter('passwordhash', '=', passwordhash)
+	
+	#call the query fetch and return User object if we find the user in datastore
+    for user in q.fetch():
+        return dataClasses.User(user['username'], user['firstname'], user['email'])
+    return None
+
+
+# loads datastore key using given client, kind, and id if provided
+def load_key(client, kind, entity_id=None, parent_key=None):
+	#if we do not pass in an id, it is none, else it is what we set is as
+	# id is the "name" (string or int) can be auto generated as an int if not given
+	# the key is the identifier, which is made of a kind and a name(id)
+	
+    key = None
+    if entity_id: #if we are given an id, create key with it
+        key = client.key(kind, entity_id, parent=parent_key)
+    else: # if we are not given an id, we will let datastore generate an int for us
+        key = client.key(kind)
+    return key
+
+
+"""
+def create_data():
+    """You can use this function to populate the datastore with some basic
+    data."""
+
+    client = _get_client()
+    entity = datastore.Entity(client.key(_USER_ENTITY, 'testuser'),
+                              exclude_from_indexes=[])
+    entity.update({
+        'username': 'testuser',
+        'passwordhash': '',
+        'email': '',
+        'about': '',
+        'completions': [],
+    })
+    client.put(entity)
+
+    entity = datastore.Entity(client.key(_COURSE_ENTITY, 'Course01'),
+                              exclude_from_indexes=['description', 'code'])
+    entity.update({
+        'code': 'Course01',
+        'name': 'First Course',
+        'description': 'This is a description for a test course.  In the \
+future, real courses will have lots of other stuff here to see that will tell \
+you more about their content.',
+    })
+    client.put(entity)
+    entity = datastore.Entity(client.key(_COURSE_ENTITY, 'Course02'),
+                              exclude_from_indexes=['description', 'code'])
+    entity.update({
+        'code': 'Course02',
+        'name': 'Second Course',
+        'description': 'This is also a course description, but maybe less \
+wordy than the previous one.'
+    })
+    client.put(entity)
+    entity = datastore.Entity(client.key(_COURSE_ENTITY,
+                                         'Course01',
+                                         _LESSON_ENTITY),
+                              exclude_from_indexes=['content', 'title'])
+    entity.update({
+        'title': 'Lesson 1: The First One',
+        'content': 'Imagine there were lots of video content and cool things.',
+    })
+    client.put(entity)
+    entity = datastore.Entity(client.key(_COURSE_ENTITY,
+                                         'Course01',
+                                         _LESSON_ENTITY),
+                              exclude_from_indexes=['content', 'title'])
+    entity.update({
+        'title': 'Lesson 2: Another One',
+        'content': '1<br>2<br>3<br>4<br>5<br>6<br>7<br>8<br>9<br>10<br>11',
+    })
+    client.put(entity)
+    entity = datastore.Entity(client.key(_COURSE_ENTITY,
+                                         'Course02',
+                                         _LESSON_ENTITY),
+                              exclude_from_indexes=['content', 'title'])
+    entity.update({
+        'title': 'Lesson 1: The First One, a Second Time',
+        'content': '<p>Things</p><p>Other Things</p><p>Still More Things</p>',
+    })
+
+    client.put(entity)
+    entity = datastore.Entity(client.key(_COURSE_ENTITY,
+                                         'Course02',
+                                         _LESSON_ENTITY),
+                              exclude_from_indexes=['content', 'title'])
+    entity.update({
+        'title': 'Lesson 2: Yes, Another One',
+        'content': '<ul><li>a</li><li>b</li><li>c</li><li>d</li><li></ul>',
+    })
+    client.put(entity)
+"""
--- main.py
+++ main.py
@@ -1,16 +1,101 @@
 from flask import Flask, Response, render_template, request
+
 from darksky.api import DarkSky
 from darksky.types import languages, units, weather
-import datetime
+
+from email.utils import parseaddr
+import datastoreHelper
+import dataClasses
+import hashlib
 import json
+
 app = Flask(__name__)
 
+#week 6 project 9 is really good for getting us setup with 
+#datastore, login, sessions
 
-# Loads the main homepage
+
+# Main homepage - do we need to expand this for differing the main page based on session? Might just go in the html
 @app.route('/')
 @app.route('/home.html')
-def home():
-    return render_template('home.html', page_title='Home')
+def root():
+    return show_page('home.html', page_title='Home')
+
+
+# route to sign up page
+@app.route('/signup')
+def signup():
+    return show_page('signup.html', page_title='Sign Up')
+
+
+# processes user registration from sign up page
+@app.route('/registerUser', methods=['POST'])
+def register_user():
+	# from the signup form - get username, pass1/2, email
+    username = flask.request.form.get('username')
+	firstname = flask.request.form.get('firstname')
+    password1 = flask.request.form.get('password1')
+    password2 = flask.request.form.get('password2')
+    email = flask.request.form.get('email')
+    errors = []
+	
+	# if passes don't match - this is an error
+    if password1 != password2:
+        errors.append('Passwords do not match.')
+	
+	# if email is entered invalid - this is an error
+    email_parts = parseaddr(email)
+    if len(email_parts) != 2 or not email_parts[1]:
+        errors.append('Invalid email address: ' + str(email))
+
+	# use the entered info to create a user object
+    user = dataClasses.User(username, firstname, email)
+    
+	# if there were any errors in the form - we will return them to the signup and tell them
+	if errors:
+        return show_page('/signup.html', 'Sign Up', errors=errors)
+    else: # else if there are no erorrs, hash password, and write the user to the db
+        passwordhash = hash_password(password1)
+        
+		# store User and hashed pasword into the datastore
+		datastoreHelper.save_user(user, passwordhash)
+        
+		# set the session user
+		flask.session['user'] = user.username
+        return flask.redirect('/')
+
+
+# route to sign in page
+@app.route('/signin')
+def signin():
+    return show_page('signin.html', page_title='Sign In')
+
+
+# processes login attempt from sign in form
+@app.route('/login', methods=['POST'])
+def login():
+	# from the sign in form, get username and password
+    username = flask.request.form.get('username')
+    password = flask.request.form.get('password')
+	
+	# hash the password
+    passwordhash = hash_password(password)
+    
+	# try to get a user object from the datastore with this username and hashpass
+	user = datastoreHelper.load_user(username, passwordhash)
+    
+	if user: # if we get a real user, this user is logged in, set the session user
+        flask.session['user'] = user.username
+        return flask.redirect('/')
+    else: # if user is not real, return them to login
+		errors = ['Failed to log in']
+        return show_page('signin.html', page_title='Sign In', errors)
+
+
+# route for G login --- Work glogin into sign in if we can / want glogin
+@app.route('/glogin')
+def gotoGlogin():
+	return render_template('glogin.html', page_title='Login')
 
 
 # Routes to the users wardrobe
@@ -35,20 +120,13 @@ def weth():
         exclude=[weather.MINUTELY, weather.DAILY, weather.ALERTS]  # exlclude minute/daily/alerts, we want to grab only what we need
     )
     # this does not work - we might have to construct a custom object/dict from this in terms of a string and send that?
-    # responseJson = json.dumps(forecast)
+    # might make an intermediate class that we can use to make these weather classes able to be written to json
+	# responseJson = json.dumps(forecast)
     # return Response(responseJson, mimetype='application/json')
 
+    # [forecast.currently.time, forecast.currently.temperature, forecast.currently.apparent_temperature]
 
-    # list = [forecast.currently.time, forecast.currently.temperature, forecast.currently.apparent_temperature]
-
-    # WHY do we get a bad gateway?
     return 'Current temperature in Pittsburgh: %s degrees' % forecast.currently.temperature
-
-
-# test html form page that redirects to result.html
-@app.route('/form')
-def student():
-    return render_template('student.html')
 
 
 # from the ajax example5, index has an AJAX button that loads content by calling to /get-data
@@ -68,31 +146,31 @@ def get_data():
     return Response(responseJson, mimetype='application/json')
 
 
-# reults page that presents the form 
-@app.route('/result', methods=['POST', 'GET'])
-def result():
-    if request.method == 'POST':
-        req_dic = request.form.to_dict()
-        print('result:{}'.format(req_dic))
-        return render_template("result.html", result=req_dic)
+# when a user signs in, the username is in the session[user], so we can get it at any time
+def get_user():
+    return flask.session.get('user', None)
 
 
-# date page that shows the current date
-@app.route('/date')
-def date():
-    return str(datetime.date.today())
+# adapted from week6p9, show page is a wrapper for render_template, it allows us to easily specify what we pass to the template
+def show_page(page, title, show=True, errors=None):
+    # removed a lot of the show page fields, and thus the render fields
+	# as we decide what needs to be passed to templates, we will add back in with this
+	# same format
+	
+	#always need a page, page title, and user to pass into template
+	#show?
+	#errors is an array of error strings, to be displayed in the errors field in the event of errors *note: errors*
+	return flask.render_template(page,
+                                 page_title=title,
+                                 user=get_user(),
+                                 show=show,
+                                 errors=errors)
 
 
-# test of putting a variable into the path
-@app.route('/hello/<name>')
-def say_hello(name):
-    return 'Hello %s!' % name
-
-
-# test of putting an int variable into the path
-@app.route('/num/<int:id>')
-def number(id):
-    return 'The number you entered is %d, but you already knew that' % id
+# Hashes the password using sha256 from hashlib
+def hash_password(pw):
+    encoded = pw.encode('utf-8')
+    return hashlib.sha256(encoded).hexdigest()
 
 
 if __name__ == '__main__':

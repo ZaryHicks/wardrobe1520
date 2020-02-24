d069214ff5853c1689d8d8ceb31614aec4ddf58c
Landon Higinbotham <lch43@pitt.edu>
2020-02-24 00:09:02 -0500

    Added Clothing class and its subclasses

--- dataClasses.py
+++ dataClasses.py
@@ -1,31 +1,83 @@
 # User object. Has a username, name, and email
 class User(object):
-	def __init__(self, username, firstname, email):
-		self.username = username
-		self.firstname = firstname
-		self.email = email
+    def __init__(self, username, firstname, email):
+        self.username = username
+        self.firstname = firstname
+        self.email = email
 
-	def to_dict(self):
-		return {
-			'username': self.username,
-			'firstname': self.firstname,
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'firstname': self.firstname,
             'email': self.email,
         }
+
 
 # Clothing object (WIP) - Do we have different subclasses? 
 class Item(object):
     def __init__(self, name):
         self.name = name
-    
+
     def to_dict(self):
         return {
             'name': self.name,
+            }
+
+
+# Potential setup for the clothing classes.
+class Clothing(object):
+    def __init__(self, name, color, is_casual, high_temp, low_temp):
+        self.name = name
+        self.color = color
+        self.isCasual = is_casual
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'color': self.name,
+            'is_casual': self.name,
+            'high_temp': self.name,
+            'low_temp': self.name,
         }
 
-# say our clothes options are: coat/jacket, top, bottom, shoes?
 
-# Subclasses of Item?
-#class jacket(item):
-#class shirt(item):
-#class pants(item):
-#class shoes(item):
+class Jacket(Clothing):
+    def __init__(self, name, color, is_casual, high_temp, low_temp):
+        super().__init__(name, color, is_casual, high_temp, low_temp)
+
+
+class Shirt(Clothing):
+    def __init__(self, name, color, is_casual, high_temp, low_temp, is_long):
+        super().__init__(name, color, is_casual, high_temp, low_temp)
+        self.is_long = is_long
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'color': self.name,
+            'is_casual': self.name,
+            'high_temp': self.name,
+            'low_temp': self.name,
+            'is_long': self.is_long
+        }
+
+
+class Pants(Clothing):
+    def __init__(self, name, color, is_casual, high_temp, low_temp, is_long):
+        super().__init__(name, color, is_casual, high_temp, low_temp)
+        self.is_long = is_long
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'color': self.name,
+            'is_casual': self.name,
+            'high_temp': self.name,
+            'low_temp': self.name,
+            'is_long': self.is_long
+        }
+
+
+class Shoes(Clothing):
+    def __init__(self, name, color, is_casual, high_temp, low_temp):
+        super().__init__(name, color, is_casual, high_temp, low_temp)
